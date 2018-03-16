97b20eeee0800f93aba44aa606a14ee355a3ed3b
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2018-03-16 14:45:20 -0700

    ProgressBar for Timer Trigger.

--- rvncerr/orange3/tarantool/widgets/timer.py
+++ rvncerr/orange3/tarantool/widgets/timer.py
@@ -27,6 +27,7 @@ class TimerWidget(OWWidget):
     priority = 10000
 
     delay = Setting(60)
+    current = 0
 
     _trigger_value = True
 
@@ -37,19 +38,30 @@ class TimerWidget(OWWidget):
         trigger = Output('Trigger', bool)
 
     @pyqtSlot()
+    def _thread_tick(self):
+        self.progressBarSet(100 * self.current / self.delay)
+
+    @pyqtSlot()
     def _thread_trigger(self):
-        self.Outputs.trigger.send(self._trigger_value)
+            self.Outputs.trigger.send(self._trigger_value)
 
     def _thread_func(self):
+        _thread_tick = methodinvoke(self, "_thread_tick", ())
         _thread_trigger = methodinvoke(self, "_thread_trigger", ())
         while True:
-            _thread_trigger(self)
-            self._trigger_value = not self._trigger_value
-            time.sleep(self.delay)
+            _thread_tick(self)
+            time.sleep(1)
+            self.current = self.current + 1
+            if self.current >= self.delay:
+                _thread_trigger(self)
+                self._trigger_value = not self._trigger_value
+                self.current = 0
 
     def __init__(self):
         super().__init__()
 
+        #self.progress = gui.ProgressBar(self, 1)
+        self.progressBarInit()
         delay_hslider = gui.hSlider(self.controlArea, self, 'delay', minValue=1, maxValue=60)
 
         self.thread = threading.Thread(target=self._thread_func)
